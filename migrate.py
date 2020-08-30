a9fc1232f9ff05986ecc939bff4d047d014e822d
unknown <mrios0495@gmail.com>
2020-08-30 16:00:20 -0400

    renames variables from client/Mobileclient to gpm_client/GpmClient

--- migrate.py
+++ migrate.py
@@ -2,26 +2,29 @@ import constants
 import json
 import spotipy
 from os import path
-from gmusicapi import Mobileclient
+from gmusicapi import Mobileclient as GpmClient
+from spotipy.oauth2 import SpotifyOAuth
 
-client = Mobileclient()
+gpm_client = GpmClient()
 
 if not path.exists(constants.CREDS):
-  client.perform_oauth(constants.CREDS)
+  gpm_client.perform_oauth(constants.CREDS)
 
-client.oauth_login(Mobileclient.FROM_MAC_ADDRESS, constants.CREDS)
-artists = []
+gpm_client.oauth_login(GpmClient.FROM_MAC_ADDRESS, constants.CREDS)
+gpm_artists = []
 
 ## Retrieve songs and filter down to unique artists
-if client.is_authenticated():
-  songs = client.get_all_songs()
+if gpm_client.is_authenticated():
+  songs = gpm_client.get_all_songs()
 
   for song in songs:
-    if song["artist"] not in artists:
-      artists.append(song["artist"])
+    if song["artist"] not in gpm_artists:
+      gpm_artists.append(song["artist"])
 else:
-  raise Exception("MobileClient is not authenticated. Credentials may have expired, delete and retry.")
+  raise Exception("GpmClient is not authenticated. Credentials may have expired, delete and retry.")
 
-artists.sort()
+gpm_artists.sort()
+print(gpm_artists)
+
 
 
