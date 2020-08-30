85647f03f4ae2f68e63cee6152842d1433d01444
unknown <mrios0495@gmail.com>
2020-08-30 16:15:47 -0400

    adds new credential constants and username

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
-CREDS = "./credentials"
+GPM_CREDS = "./gpm_credentials"
+SPOTIFY_CREDS = "./spotify_credentials"
 LOG = "./log.txt"
 FOLLOW_SCOPE = "user-follow-modify"
 
 ## Retrieve these from the spotify dev dashboard
 CLIENT_ID = ""
 CLIENT_SECRET = ""
+CLIENT_USER = ""
